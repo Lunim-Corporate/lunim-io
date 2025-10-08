e08eb82a14004fee32762015102ec8201158e98a
Asish <66243448+assh@users.noreply.github.com>
2025-10-08 14:15:15 +0100

    feat: Add image remote patterns for Prismic in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.prismic.io",
+        // narrow to your repo (optional but safer):
+        pathname: "/lunim-v3/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
