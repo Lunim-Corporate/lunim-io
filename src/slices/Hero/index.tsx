bc0db2a01ad126a834cf35b077b7981d7976835b
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-10-09 17:09:38 +0100

    feat: Add Hero slice component with model and mock data

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <div><pre>{JSON.stringify(slice, null, 2)}</pre></div>
+  );
+};
+
+export default Hero;
