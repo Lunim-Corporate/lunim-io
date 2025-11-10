59f1047e44053790be4123c6268899088fdddfb8
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-11-10 16:05:07 +0000

    Refactor metadata generation functions to enhance SEO and improve data fetching logic

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -4,9 +4,9 @@ import { SliceZone } from "@prismicio/react";
 import type { Content } from "@prismicio/client";
 import { components } from "@/slices";
 // Next
-import { ResolvingMetadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
 // Utils
-import { getMetaDataInfo } from "@/utils/metadata";
+import { pickBaseMetadata } from "@/utils/metadata";
 
 export const revalidate = 60;
 
@@ -33,8 +33,57 @@ export default async function Page() {
   );
 }
 
-export async function generateMetadata(_context: unknown, parent: ResolvingMetadata) {
-  const pathname = "/our-team";
-
-  return getMetaDataInfo(pathname, parent);
+export async function generateMetadata(
+  _context: unknown,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // fetch data
+  const client = createClient();
+  const parentMetaData = await pickBaseMetadata(parent);
+  const doc = await client
+  .getSingle<Content.OurTeamPageDocument>("our_team_page")
+  .catch(() => null);
+  if (!doc) {
+    return {
+      title: "Lunim",
+      description: "Welcome to Lunim's official team page."
+    };
   }
+
+
+  // const parentUrl = (await parent).openGraph?.images?.[0]?.url || "";
+  // const parentAlt = (await parent).openGraph?.images?.[0]?.alt || "";
+  const parentKeywords = parentMetaData.keywords || "";
+  // Filter out empty keyword fields
+  // Ensure each keyword is separated by a comma and space
+  // Join keywords from current page (if any) to parent keywords
+  const keywords = doc.data?.meta_keywords.filter((val) => Boolean(val.meta_keywords_text)).length >= 1 ? `${parentKeywords}, ${doc.data.meta_keywords.map((k) => k.meta_keywords_text?.toLowerCase()).join(", ")}` : parentKeywords;
+  const title = doc.data?.meta_title || parentMetaData.title;
+  const description = doc.data?.meta_description || parentMetaData.description;
+  const canonicalUrl = doc.data?.meta_url || "";
+
+  return {
+    ...parentMetaData,
+    title: title,
+    description: description,
+    keywords: keywords,
+    openGraph: {
+      ...parentMetaData.openGraph,
+      title: `${title}`,
+      description: `${description}`,
+      url: canonicalUrl,
+      // images: [
+      //   {
+      //     url: `${doc.data?.meta_image}` || `${parentUrl}`,
+      //     alt: `${doc.data?.meta_image_alt_text}` || `${parentAlt}`,
+      //   }
+      // ]
+    },
+  }
+}
+
+// export async function generateMetadata(_context: unknown, parent: ResolvingMetadata) {
+//   const pathname = "/our-team";
+
+//   return getMetaDataInfo(pathname, parent);
+//   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,9 +54,10 @@ export async function generateMetadata(
   // Filter out empty keyword fields
   // Ensure each keyword is separated by a comma and space
   // Join keywords from current page (if any) to parent keywords
-  const keywords = doc.data?.meta_keywords.filter((val) => Boolean(val.meta_keywords_text)).length >= 1 ? `${doc.data.meta_keywords.map((k) => k.meta_keywords_text?.toLowerCase()).join(", ")}, ${parentKeywords}` : parentKeywords;
+  const keywords = doc.data?.meta_keywords.filter((val) => Boolean(val.meta_keywords_text)).length >= 1 ? `${parentKeywords}, ${doc.data.meta_keywords.map((k) => k.meta_keywords_text?.toLowerCase()).join(", ")}` : parentKeywords;
   const title = doc.data?.meta_title || parentMetaData.title;
   const description = doc.data?.meta_description || parentMetaData.description;
+  const canonicalUrl = doc.data?.meta_url || "";
 
   return {
     ...parentMetaData,
@@ -67,7 +68,7 @@ export async function generateMetadata(
       ...parentMetaData.openGraph,
       title: `${title}`,
       description: `${description}`,
-      // url: canonical,
+      url: canonicalUrl,
       // images: [
       //   {
       //     url: `${doc.data?.meta_image}` || `${parentUrl}`,
