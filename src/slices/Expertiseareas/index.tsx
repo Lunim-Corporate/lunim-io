72b94cb2066477dc56a8d594823dbdbe44bcc5b5
hubagent <pete@cahootify.com>
2025-11-13 15:41:51 +0000

    Max at three cards

--- src/slices/Expertiseareas/index.tsx
+++ src/slices/Expertiseareas/index.tsx
@@ -157,7 +157,7 @@ const Expertiseareas: FC<ExpertiseareasProps> = ({ slice }) => {
               }}
             />
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {items.map(renderCard)}
           </div>
         </div>
