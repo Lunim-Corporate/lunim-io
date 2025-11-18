cdfc6167aa58a059b2ad5fea7502ceb939a5e2bf
Asish <66243448+assh@users.noreply.github.com>
2025-11-18 11:10:02 +0000

    Updated helper file

--- src/components/JsonLd.tsx
+++ src/components/JsonLd.tsx
@@ -1,10 +1,16 @@
-// src/components/JsonLd.tsx
+"use client";
+
 import type { WithContext } from "schema-dts";
 
-export function JsonLd<T extends WithContext<any>>({ data }: { data: T }) {
+interface JsonLdProps<T extends WithContext<any>> {
+  data: T;
+}
+
+export function JsonLd<T extends WithContext<any>>({ data }: JsonLdProps<T>) {
   return (
     <script
       type="application/ld+json"
+      // JSON.stringify makes sure it's valid JSON, not JS
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   );
