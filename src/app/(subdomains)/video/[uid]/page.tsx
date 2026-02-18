eb1fa2eb1196e2c332089cf08589d9c54ce84685
Asish <66243448+assh@users.noreply.github.com>
2026-02-18 11:22:01 +0000

    Return 404 for missing subsite pages

--- src/app/(subdomains)/video/[uid]/page.tsx
+++ src/app/(subdomains)/video/[uid]/page.tsx
@@ -7,6 +7,8 @@ import { pickBaseMetadata } from "@/utils/metadata";
 import { generateMetaDataInfo } from "@/utils/generateMetaDataInfo";
 import type { VideoPageDocument } from "../../../../../prismicio-types";
 
+export const dynamic = "force-dynamic";
+
 type Params = { uid: string };
 
 export default async function VideoDynamicPage({
